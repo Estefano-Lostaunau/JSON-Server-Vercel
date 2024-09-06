ccd8bf15f1d2f5f8e275d1d5e95350a5172d77eb
Estefano-Lostaunau <u202211742@upc.edu.pe>
2024-09-06 07:57:10 -0500

    .

--- api/server.js
+++ api/server.js
@@ -1,31 +1,19 @@
 // See https://github.com/typicode/json-server#module
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults();
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
 
-server.use(middlewares);
-server.use(jsonServer.bodyParser);
-
-// Rewriter rules
+server.use(middlewares)
+// Add this before server.use(router)
 server.use(jsonServer.rewriter({
     '/api/*': '/$1',
     '/product/:resource/:id/show': '/:resource/:id'
-}));
-
-// Allow write operations
-server.use((req, res, next) => {
-    if (['POST', 'PUT', 'PATCH', 'DELETE'].includes(req.method)) {
-        next();
-    } else {
-        next();
-    }
-});
-
-server.use(router);
-
+}))
+server.use(router)
 server.listen(3000, () => {
-    console.log('JSON Server is running');
-});
+    console.log('JSON Server is running')
+})
 
-module.exports = server;
+// Export the Server API
+module.exports = server
