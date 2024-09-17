4faedfe3bdb437d2692b0d591b02fe93721b9dc7
Estefano-Lostaunau <u202211742@upc.edu.pe>
2024-09-17 13:15:06 -0500

    .

--- api/server.js
+++ api/server.js
@@ -8,7 +8,7 @@ const fs = require('fs')
 const path = require('path')
 const filePath = path.join('db.json')
 const data = fs.readFileSync(filePath, "utf-8");
-const db = JSON.parse(data);
+const db = JSON.parse(fs.readFileSync(path.join("db.json")))
 const router = jsonServer.router(db)
 
 // Comment out to allow write operations
